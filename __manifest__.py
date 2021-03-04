9e03e2bd054714f9d5387299b34b9fb5cbe0dd98
yanuaar <yanuarpradana7@gmail.com>
2021-03-04 10:12:22 +0700

    First Commit

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name" : "Report Custom",
+    "version" : "1.0",
+    "depends" : ["base", "purchase", "account"],
+    "author" : "SATU SOFT",
+    "category" : "Satu Soft Modul",
+    "website" : "satusoft.com",
+    "description" : "Report Custom",
+    "data" : [
+        "reports/inv_report_wan2try.xml",
+        "reports/inv_report_wan2try_document.xml",
+        "reports/inv_report_cekne.xml",
+        "reports/inv_report_cekne_document.xml",
+        "reports/inv_report_inherit.xml",
+        "reports/report_coba.xml",
+        "reports/report_coba_document.xml",
+    ],
+}
