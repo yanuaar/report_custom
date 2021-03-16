7d881b96b5c97444867ec6f3c5b6aecb33b9f747
Yanuar Pradana <yanuar.ryp@gmail.com>
2021-03-16 10:47:40 +0700

    First Commit

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     "author" : "SATU SOFT",
     "category" : "Satu Soft Modul",
     "website" : "satusoft.com",
-    "description" : "Report Custom",
+    "description" : "Report Custom Invoice Wan2Try dan Cekne",
     "data" : [
         "reports/inv_report_wan2try.xml",
         "reports/inv_report_wan2try_document.xml",
