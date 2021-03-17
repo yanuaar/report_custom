34eaaf2d6d399dd93df2f6fc780b5e594460306f
Yanuar Pradana <yanuar.ryp@gmail.com>
2021-03-17 14:39:10 +0700

    Report Custom Wan2try

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,11 @@
 {
-    "name" : "Report Custom",
+    "name" : "Report Custom Wan2try",
     "version" : "1.0",
     "depends" : ["base", "purchase", "account"],
     "author" : "SATU SOFT",
     "category" : "Satu Soft Modul",
     "website" : "satusoft.com",
-    "description" : "Report Custom",
+    "description" : "Report Custom Invoice Wan2Try dan Cekne\n [ADD] - Invoices Wan2try and Cekne",
     "data" : [
         "reports/inv_report_wan2try.xml",
         "reports/inv_report_wan2try_document.xml",
